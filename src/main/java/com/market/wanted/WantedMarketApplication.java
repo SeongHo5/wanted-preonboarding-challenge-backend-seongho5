2ef12c9788da4d6c9e1707812330efff3bfc4a8c
SeongHo5 <newfuring1123@gmail.com>
2024-05-21 22:07:19 +0900

    Init project
    - 프로젝트 초기 설정

---
+++ src/main/java/com/market/wanted/WantedMarketApplication.java
@@ -0,0 +1,12 @@
+package com.market.wanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedMarketApplication.class, args);
+    }
+}
---
+++ src/test/java/com/market/wanted/WantedMarketApplicationTest.java
@@ -0,0 +1,13 @@
+package com.market.wanted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedMarketApplicationTest {
+
+    @Test
+    void contextloads() {
+    }
+
+}
