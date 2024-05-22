789f49e693336b9ca19a70b590fc5159bf1e7daa
SeongHo5 <newfuring1123@gmail.com>
2024-05-22 20:58:02 +0900

    feat: Add core configs
    - 시간 / 스레드 / JPA 설정 클래스 추가

---
+++ src/main/java/com/market/wanted/core/config/ThreadConfig.java
@@ -0,0 +1,25 @@
+package com.market.wanted.core.config;
+
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+public class ThreadConfig {
+
+    @Bean
+    protected TomcatProtocolHandlerCustomizer<?> protocolHandlerCustomizer() {
+        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+    }
+
+    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+    protected AsyncTaskExecutor asyncTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
+
+}
